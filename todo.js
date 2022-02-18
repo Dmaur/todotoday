bb7be5036cb5d54fcf0aa588e5a6caf7b6d41893
Derrick Maurais <derrickmaurais1@gmail.com>
2022-02-18 19:31:30 -0400

    having trouble getting enter to add list items like a normal submit

--- todo.js
+++ todo.js
@@ -1,9 +1,22 @@
 
- addstuff = function () {
-     document.getElementById("button").onclick = function() {
-        let text = document.getElementById("input").value;
+window.onload = function () {
+    document.getElementById("button").onclick = function () {
+        let text = document.getElementById("inp").value;
         let li = "<li>" + text + "</li>"
-        document.getElementById("listitems").insertAdjacentHTML('beforeend', li);
-        document.getElementById("input").value = "";
-}
-}
+        if (text === "") {
+            alert("please add a to do item")
+        } else {
+            document.getElementById("listitems").insertAdjacentHTML('beforeend', li);
+            document.getElementById("inp").value = "";
+            console.log(addLi())
+        }
+
+    }
+}   
+//     document.getElementById("inp").addEventListener("keyup", function (event) {
+//         if (event.code === "KeyEnter") {
+//             event.preventDefault("keyEnter");
+//             document.getElementById("button").onclick();
+//         }
+//     });
+// }
